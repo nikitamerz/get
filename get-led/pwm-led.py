2d14666f6d3dbe2f32eca394773bb999d63fdef8
R <nikitamerz2017@gmail.com>
2026-02-11 15:22:31 +0300

    добавил

---
+++ get-led/pwm-led.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+led = 26
+GPIO.setup(led, GPIO.OUT)
+pwm = GPIO.PWM(led, 200)
+duty = 0.0
+pwm.start(duty)
+#дьюти определяет, на сколько процентов включен светодиод
+while True:
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.05)
+
+    duty += 1
+    if duty > 100.0:
+        duty = 0.0
